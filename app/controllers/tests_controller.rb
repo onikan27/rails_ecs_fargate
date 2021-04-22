864daba5f2aac272f5d5a6633ce236f3c2f46caa
onikan27 <ude0721juji@icloud.com>
2021-04-22 14:20:43 +0900

    fix

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,3 +3,8 @@ class TestsController < ApplicationController
     head :ok
   end
 end
+# docker push 690659219943.dkr.ecr.ap-northeast-1.amazonaws.com/rails_ecs_fargate_server:latest
+# docker push 690659219943.dkr.ecr.ap-northeast-1.amazonaws.com/rails_ecs_fargate_web:latest
+# docker tag rails_ecs_fargate_web:latest 690659219943.dkr.ecr.ap-northeast-1.amazonaws.com/rails_ecs_fargate_web:latest
+# docker tag rails_ecs_fargate_server:latest 690659219943.dkr.ecr.ap-northeast-1.amazonaws.com/rails_ecs_fargate_server:latest
+
